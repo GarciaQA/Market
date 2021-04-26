2d0bfa702d67136e7d91537c8d02e369f9a6a478
Adrian Garcia <agarcia@inove.local>
2021-04-26 10:09:31 -0500

    changes 2.02

--- js/acordion.js
+++ js/acordion.js
@@ -1,24 +1,17 @@
 new Swiper('.main-banner', {
-    // Optional parameters
+    slidesPerView: 1,
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
     },
     direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
     pagination: {
       el: '.swiper-pagination',
     },
-  
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
-    // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
     },
